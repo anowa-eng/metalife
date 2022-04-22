2d708cf6f465e321a631c82a8c571fb9f08b493d
anowa-eng <georgemorrissette2010@gmail.com>
2022-04-22 00:45:17 +0000

    more switch.sh

--- switch.sh
+++ switch.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
+APP=/workspace/metalife
+BACKEND=$APP/backend
+FRONTEND=$APP/frontend
 
 backend() {
-    cd /workspace/metalife/backend/ || exit
+    cd $BACKEND || exit
 
     cd venv || exit
     source bin/activate
 
     cd project || exit
+
+    while getopts 'r' opt; do
+        if [[ $opt == 'r' ]]; then
+            python3 manage.py runserver
+        fi
+    done
 }
 
 frontend() {
-    cd /workspace/metalife/frontend/ || exit
+    cd $FRONTEND || exit
     
     cd venv || exit
     source bin/activate
+
+    cd components/src/app/ || exit
+    ng build --base-href . --output-url $BACKEND/project/app/static/ang/ --output-hashing none --watch
 }
 
 
