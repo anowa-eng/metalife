6e8cc2583da226cdb9afb1349b8c97c93f7a193e
anowa-eng <georgemorrissette2010@gmail.com>
2022-04-23 17:34:00 +0000

    Build

--- backend/venv/project/app/views.py
+++ backend/venv/project/app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
 def index_view(req):
     return render(req, 'index.html')
+def current_room(req):
+    current_room = req.session.get('current_room')
+    return HttpResponse(current_room if current_room else '')
--- backend/venv/project/project/urls.py
+++ backend/venv/project/project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app.views import *
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', index_view),
+    path('/api/currentroom', current_room)
 ]
