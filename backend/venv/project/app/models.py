843fb8f3fbafdac407acc4b0f8a24128eafbdc2d
anowa-eng <georgemorrissette2010@gmail.com>
2022-05-01 15:55:13 -0700

    Add models

--- backend/venv/project/app/models.py
+++ backend/venv/project/app/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
 class User(models.Model):
-    username = models.CharField(max_length=250)
+    username = models.CharField(max_length=255)
     password = models.TextField()
 class UserProfile(models.Model):
     profile_picture = models.ImageField(upload_to='media/')
 
     user = models.ForeignKey(User, on_delete=User)
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+class InvitedUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+class UserPosition(models.Model):
+    x = models.PositiveBigIntegerField()
