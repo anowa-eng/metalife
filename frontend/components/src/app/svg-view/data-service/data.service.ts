ef9272f79105e01d8d4a651ab86f794a52cb9f76
anowa-eng <georgemorrissette2010@gmail.com>
2022-05-08 09:20:04 -0700

    Add more to data.service.ts, add @ts-ignore line

--- frontend/components/src/app/svg-view/data-service/data.service.ts
+++ frontend/components/src/app/svg-view/data-service/data.service.ts
@@ -14,6 +14,7 @@ export class DataService {
 
   room?: number;
 
+  // @ts-ignore
   constructor(private httpService: HttpService, private webSocketService: WebSocketService) {
     // Get the "initial data"
     this.getInitialData()
