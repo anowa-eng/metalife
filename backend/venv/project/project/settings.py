b1a92b4c403243a5d112441f431ecde60addf450
anowa-eng <georgemorrissette2010@gmail.com>
2022-05-08 14:06:51 -0700

    Fix errors

--- backend/venv/project/project/settings.py
+++ backend/venv/project/project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['app'],
+        'DIRS': ['app/app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- switch.sh
+++ switch.sh
@@ -19,7 +19,7 @@ backend() {
 frontend() {
   cd $FRONTEND/components/src/app/ || exit
 
-  if [[ $0 = 'build' ]]; then
+  if [[ $0 == 'build' ]]; then
       ng build --base-href . --output-path $BACKEND/app/static/ang/ --output-hashing none --watch
   fi
 }
