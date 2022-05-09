3df9b1b73310442bd8653530ac12fa9c8c487089
anowa-eng <georgemorrissette2010@gmail.com>
2022-05-08 18:18:36 -0700

    Add string methods

--- backend/venv/project/app/models.py
+++ backend/venv/project/app/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=255)
     password = models.TextField()
+
+    def __str__(self):
+        return self.username
 class UserProfile(models.Model):
     profile_picture = models.ImageField(upload_to='media/')
 
@@ -12,12 +15,25 @@ class UserProfile(models.Model):
 
 class Room(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 class InvitedUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 class UserPosition(models.Model):
     x = models.PositiveBigIntegerField()
     y = models.PositiveBigIntegerField()
 class UserInRoom(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
     user_position = models.ForeignKey(UserPosition, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} in {self.room.name}'
+    class Meta:
+        verbose_name_plural = 'users in rooms'
