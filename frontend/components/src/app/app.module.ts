f7a8aff6c17b2fc66ff1be3cc6c272d6bee6f396
anowa-eng <georgemorrissette2010@gmail.com>
2022-05-15 17:31:19 -0700

    Fix error

--- frontend/components/src/app/app.module.ts
+++ frontend/components/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http/'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomViewComponent } from './room-view/svg-view.component';
@@ -12,6 +14,8 @@ import { RoomViewComponent } from './room-view/svg-view.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    
     AppRoutingModule
   ],
   providers: [],
