d243146c6e6e9e9493aa4d463a50ae03de0dd5f4
anowa-eng <georgemorrissette2010@gmail.com>
2022-05-15 17:32:32 -0700

    Fix typo

--- frontend/components/src/app/app.module.ts
+++ frontend/components/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http/'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +15,7 @@ import { RoomViewComponent } from './room-view/svg-view.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    
+
     AppRoutingModule
   ],
   providers: [],
