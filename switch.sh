1631b7bb408a944ab4f6d95ce1680c2afb4408ff
anowa-eng <georgemorrissette2010@gmail.com>
2022-05-17 16:23:56 -0700

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -19,7 +19,7 @@ backend() {
 frontend() {
   cd $FRONTEND/components/src/app/ || exit
 
-  if [[ $0 == 'build' ]]; then
+  if [[ $1 == 'build' ]]; then
       ng build --base-href . --output-path $BACKEND/venv/project/app/static/ang/ --output-hashing none --watch
   fi
 }
